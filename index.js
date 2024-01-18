ecfca0d432277b8b0c8e723f18e82a40a75f3406
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-18 15:28:43 +0530

    modified index.js and finalised env file

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const app = express();
 
 const userRoutes = require("./routes/User");
 const database = require("./config/database");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 4000
+
+database.connect();
+
+app.use("/api/v1/user",userRoutes);
+
+app.get("/",(req,res) => {
+    return res.status(200).json({
+        success:true,
+        message:"Your server is up and running...",
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`App is running at ${PORT}`)
+})
+
